6a630f29217217064d9c950f8a82778961789be4
Taras Panasiuk <taras.panasiuk@quintagroup.org>
2025-03-05 23:50:49 +0200

    feat: Add face registration page with webcam capture functionality

--- frontend/src/router/Routes.jsx
+++ frontend/src/router/Routes.jsx
@@ -4,6 +4,8 @@ import { HomePage } from '@/pages/HomePage';
 import { ConfirmSignup } from '@/pages/auth/ConfirmSignup';
 import { Signup } from '@/pages/auth/Signup';
 import { Signin } from '@/pages/auth/Signin';
+import { FaceRegistration } from '@/pages/auth/FaceRegistration';
+
 
 export const router = createBrowserRouter([
   {
@@ -21,5 +23,9 @@ export const router = createBrowserRouter([
   {
     path: '/signin',
     element: <Signin />,
+  },
+  {
+    path: '/face-registration',
+    element: <FaceRegistration />,
   },
 ]);
