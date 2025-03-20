b03f859393da839d60791a14a95facb021ef6585
Taras Panasiuk <taras.panasiuk@quintagroup.org>
2025-03-21 01:22:03 +0200

    feat: Setup Gitlab CI/CD

--- infrastructure/build-and-push-images.sh
+++ infrastructure/build-and-push-images.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
+set -e
+
 echo "Logging in to ECR"
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 
 echo "Building the Backend Docker image"
 docker build --no-cache -t $AWS_ECR_REPOSITORY_NAME:$BACKEND_TAG -f backend/Dockerfile ./backend
 
+echo "Building the Frontend Docker image"
+docker build --no-cache -t $AWS_ECR_REPOSITORY_NAME:$FRONTEND_TAG -f frontend/Dockerfile ./frontend
+
 echo "Tagging the Backend Docker image"
 docker tag $AWS_ECR_REPOSITORY_NAME:$BACKEND_TAG $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$AWS_ECR_REPOSITORY_NAME:$BACKEND_TAG
 
+echo "Tagging the Frontend Docker image"
+docker tag $AWS_ECR_REPOSITORY_NAME:$FRONTEND_TAG $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$AWS_ECR_REPOSITORY_NAME:$FRONTEND_TAG
+
 echo "Pushing the Backend Docker image to ECR"
 docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$AWS_ECR_REPOSITORY_NAME:$BACKEND_TAG
+
+echo "Pushing the Frontend Docker image to ECR"
+docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$AWS_ECR_REPOSITORY_NAME:$FRONTEND_TAG
